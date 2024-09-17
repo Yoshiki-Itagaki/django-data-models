a6fecffc0b23ffadf35977027943ac6dd17cde25
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-17 22:50:37 +0900

    Updated models and actual data

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,4 +12,5 @@ class Book(models.Model):
     is_best_selling = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.title} ({self.rating})"
+        return f"{self.title} ({self.rating}), written by {self.author}, which is best-selling {self.is_best_selling} "
+    
