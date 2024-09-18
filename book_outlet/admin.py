a4e15ca1e0ffe30e99291969d4f4a93193641fdc
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-18 22:38:17 +0900

    Configured the admin setting

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Book
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug", )
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,14 +12,14 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", blank=True, null=False, editable=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
     
 
     def __str__(self):
