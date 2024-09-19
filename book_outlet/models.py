e1a36db09efcd3302bc371b5b74e667a790f3010
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-19 21:38:58 +0900

    Added a one-to-one relation

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,9 +5,16 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    
     
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
