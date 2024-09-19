ba4e585bc916a9785444172e6b4adf8b77871b2d
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-19 23:20:19 +0900

    Configured a many-to-many relationship

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Address, Author, Book
+from .models import Address, Author, Book, Country
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Address)
+admin.site.register(Country)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,6 +5,16 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -40,6 +50,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country,)
+    # published_countries = models.ManyToManyField(Country, related_name="books"
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
